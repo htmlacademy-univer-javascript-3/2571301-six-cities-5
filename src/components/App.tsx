4065ffbe9d5a777f704a7591fe222c11d49a04e7
Svyatoslav <shemelin-01@mail.ru>
2024-12-15 13:18:13 +0300

    added api and simple comp tests

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -8,7 +8,7 @@ import FavouritePage from './FavouritePage/FavoritePage';
 import LoginPage from './LoginPage/LoginPage';
 import PrivateRoute from './PrivateRoute/PrivateRoute';
 import { useAppSelector } from '../hooks';
-import LoadingScreen from './LoadingPage/LoadingPage';
+import LoadingPage from './LoadingPage/LoadingPage.tsx';
 import HistoryRouter from './HistoryRouter/HistoryRouter.tsx';
 import browserHistory from '../services/browserHistory.ts';
 import { useMemo } from 'react';
@@ -41,7 +41,7 @@ function App(): JSX.Element {
 
   if (authorizationStatus === AuthorizationStatus.Unknown || isDataLoading) {
     return (
-      <LoadingScreen />
+      <LoadingPage />
     );
   }
 
